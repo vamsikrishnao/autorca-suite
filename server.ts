9c4b0c861bf72fd1bb91a1d042af2f8858bcd68b
vamsikrishnao <ovamsikrishna@gmail.com>
2026-08-05 19:10:07 +0530

    Update Port conflict situation when used by apps

--- server.ts
+++ server.ts
@@ -140,7 +140,7 @@ function pruneExpiredWorkerJobs(): number {
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
   app.use(express.json());
 
